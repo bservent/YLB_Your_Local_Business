66ea72391d170a055545ef66ade71066105d3004
edambal <bal818edam@gmail.com>
2020-10-09 17:05:14 -0700

    DB Connected Successfully

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('/businesses/index');
+    res.render('businesses/index');
 })
 
 module.exports = router;
