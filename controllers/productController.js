c0053077239ec8c05849663d9ad3696f4ca85eb5
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-10-11 21:34:12 -0700

    create route

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const db = require('../models');
 
 //index route
-
 router.get('/', (req, res) => {
     db.Product.find({}, (err, allProducts) => {
     if (err) return console.log(err);
@@ -17,5 +16,29 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('products/new');
 });
+
+//show route
+router.get('/:productId', (req, res) => {
+    db.Product.findById(req.params.productId, (err, foundProduct) => {
+        if (err) return console.log(err);
+        res.render('products/show', {
+        product: foundProduct
+        });
+    });
+});
+
+//create route - POST
+
+router.post('/', (req, res) => {
+    db.Product.create(req.body, (err, newProduct) => {
+        db.Product.find({}, (err, allProducts) => {
+            if (err) return console.log(err);
+            res.render('products',{
+                products: allProducts
+            });
+        });
+    });
+});
+
 
 module.exports = router;
